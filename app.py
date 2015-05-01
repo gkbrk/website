693df9721eef3fcd69a02f4311f39a64cec853c0
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-05-01 22:05:19 +0300

    Refactored static files.

--- app.py
+++ app.py
@@ -12,8 +12,12 @@ def cv():
     with open("cv.tpl") as template:
         return bottle.template(template.read())
 
+@bottle.get("/static/<filename:path>")
+def static(filename):
+    return bottle.static_file(filename, root="static/")
+
 application = bottle.default_app()
-application.install(bottlog.apache_logger)
+application.install(bottlog.apache_logger("server.log"))
 
 if __name__ == "__main__":
     bottle.run(app=application, port=1233, server="paste")
--- bottlog.py
+++ bottlog.py
@@ -1,54 +1,37 @@
 import bottle
 import time
 
-def apache_logger(callback):
-    """Logs in the combined log format"""
-    def wrapper(*args, **kwargs):
-        body = callback(*args, **kwargs)
+class apache_logger:
+    def __init__(self, logfile):
+        self.name = "ApacheLogger"
+        self.api = 2
+        self.logfile = logfile
 
-        log_data = []
-        log_data.append(bottle.request.remote_addr)
-        log_data.append("-")
-        log_data.append("-")
-        log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
-        log_data.append('"%s %s %s"' % (bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
-        log_data.append(str(bottle.response.status_code))
-        log_data.append(str(len(body) if not isinstance(body, bottle.HTTPResponse) else 0))
+    def apply(self, callback, route=None):
+        def wrapper(*args, **kwargs):
+            body = callback(*args, **kwargs)
 
-        referer = bottle.request.get_header("Referer", "-")
-        if referer != "-":
-            referer = "\"%s\"" % referer
-        log_data.append(referer)
+            log_data = []
+            log_data.append(bottle.request.remote_addr)
+            log_data.append("-")
+            log_data.append("-")
+            log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
+            log_data.append("\"{} {} {}\"".format(bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
+            log_data.append(str(bottle.response.status_code))
+            log_data.append(str(len(body) if not isinstance(body, bottle.HTTPResponse) else 0))
 
-        user_agent = bottle.request.get_header("User-Agent", "-")
-        if user_agent != "-":
-            user_agent = "\"%s\"" % user_agent
-        log_data.append(user_agent)
+            referer = bottle.request.get_header("Referer", "-")
+            if referer != "-":
+                referer = "\"{}\"".format(referer)
+            log_data.append(referer)
 
-        with open("server.log", "a+") as log_file:
-            log_file.write(" ".join(log_data) + "\n")
+            user_agent = bottle.request.get_header("User-Agent", "-")
+            if user_agent != "-":
+                user_agent = "\"{}\"".format(user_agent)
+            log_data.append(user_agent)
 
-        return body
+            with open(self.logfile, "a+") as log_file:
+                log_file.write("{}\n".format(" ".join(log_data)))
 
-    return wrapper
-
-def common_logger(callback):
-    """Logs in the common log format"""
-    def wrapper(*args, **kwargs):
-        body = callback(*args, **kwargs)
-
-        log_data = []
-        log_data.append(bottle.request.remote_addr)
-        log_data.append("-")
-        log_data.append("-")
-        log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
-        log_data.append('"%s %s %s"' % (bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
-        log_data.append(str(bottle.response.status_code))
-        log_data.append(str(len(body) if not isinstance(body, bottle.HTTPResponse) else 0))
-
-        with open("server.log", "a+") as log_file:
-            log_file.write(" ".join(log_data) + "\n")
-
-        return body
-
-    return wrapper
+            return body
+        return wrapper
