4285d50b9a009a66757bab1344e37343ef0aa230
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-05-01 21:46:31 +0300

    Started refactoring

---
+++ app.py
@@ -0,0 +1,14 @@
+import bottle
+import bottlog
+
+@bottle.get("/")
+@bottle.get("/index")
+def root_path():
+    with open("index.tpl") as template:
+        return bottle.template(template.read())
+
+application = bottle.default_app()
+application.install(bottlog.apache_logger)
+
+if __name__ == "__main__":
+    bottle.run(app=application, port=1233, server="paste")
